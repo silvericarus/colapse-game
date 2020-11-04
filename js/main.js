f934fbe4f31e41964cd1499a1e3166f7586677e0
Silver Icarus <albin2@yahoo.com>
2020-11-04 20:31:44 +0100

    Finish move up button function

--- js/main.js
+++ js/main.js
@@ -22,3 +22,35 @@ function rollDice8(){
 
 var diceBtn = document.getElementById("diceBtn");
 diceBtn.onclick = rollDice8;
+var btnMoveUp = document.getElementById("btnMoveUp");
+var btnMoveLeft = document.getElementById("btnMoveLeft");
+var btnMoveRight = document.getElementById("btnMoveRight");
+var btnMoveDown = document.getElementById("btnMoveDown");
+function move(dir){
+    switch(dir) {
+        case 1: //Up
+            var losPos = Array.from(human.parentNode.parentNode.parentNode.parentNode.children).findIndex(element => 
+                element.children[0].children[2].children[0] == human);
+            if (losPos != -1){
+                var losParDes = human.parentNode.parentNode.parentNode.parentNode.previousElementSibling;
+                var arrayLosParDes = Array.from(losParDes.children);
+                var losParElement;
+                losParElement = arrayLosParDes[losPos];
+                if(losParElement.children[0].children[2].classList.contains("esp")){
+                    losParElement.appendChild(human);
+                    btnMoveUp.classList.add("disabled");
+                }
+            }
+            break;
+        case 2: //Left
+            break;
+        case 3: //Right
+            break;
+        case 4: //Down
+            break;
+        default:
+            break;
+    }
+
+    
+}
